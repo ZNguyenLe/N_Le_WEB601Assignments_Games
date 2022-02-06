3f46cdad126112bcd8e74ef49d6cea2be041be9b
ZNguyenLe <nguyen.le@stclairconnect.ca>
2022-02-06 18:32:53 -0500

    worked on assignment 2, up to page displaying on page

--- N_Le_Games/src/app/content-card/content-card.component.ts
+++ N_Le_Games/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,43 +7,16 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-
-  fortnite: Content = {
-    id: 1,
-    title: "Fortnite",
-    description: "An Online Battle Royal PVP Game",
-    creator: "Epic Games",
-    imgURL: "http://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg",
-    type: "Game"
-  };
-  valorant: Content = {
-    id: 2,
-    title: "Valorant",
-    description: "An Online Shooter PVP",
-    creator: "Riot Games",
-    imgURL: "https://cdn2.steamgriddb.com/file/sgdb-cdn/logo_thumb/9ebd41e6cbc1e14780805f6fc0d65867.png",
-    type: "Game"
-  };
-  LeagueOfLegends: Content = {
-    id: 3,
-    title: "League of Legends",
-    description: "An Online 5 Moba PVP Game",
-    creator: "Riot Games",
-    imgURL: "https://preview.redd.it/itq8rpld8va51.png?width=256&format=png&auto=webp&s=9701ba6228c29bf2d7e3dfffd45b9a3562507289",
-    type: "Game"
-  };
-  
-  contentList = new ContentList();
-
-  constructor() { 
-    this.contentList.addGame(this.fortnite);
-    this.contentList.addGame(this.valorant);
-    this.contentList.addGame(this.LeagueOfLegends);
-  }
-  
-  
+  @Input() gameList?: Content;
+    constructor() {
+      
+    }
+    
+    
   ngOnInit(): void {
     
   }
-
+  onImageClick() {
+   console.log(this.gameList?.id, this.gameList?.title);
+  }
 }
--- N_Le_Games/src/app/content-list/content-list.component.ts
+++ N_Le_Games/src/app/content-list/content-list.component.ts
@@ -1,23 +1,22 @@
-//import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { Component } from '@angular/core';
-import { Content } from './helper-files/content-interface';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
 @Component({
-  selector: 'app-root', // app-content-card
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
 })
-export class AppComponent {
-  title = 'N_Le_Games';
-  lotsofgames: Content[];
-  constructor() { 
-    this.lotsofgames = [
+export class ContentListComponent implements OnInit {
+  //@Input() lotsofgames: Content[] ;
+  lotsofgames: Content[] = [
     {
       id: 1,
       title: "Fortnite",
       description: "An Online Battle Royal PVP Game",
       creator: "Epic Games",
       imgURL: "http://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg",
-      type: "Game"
+      type: "Game",
+      tags: "gaming"
     },
     {
       id: 2,
@@ -25,7 +24,9 @@ export class AppComponent {
       description: "An Online Shooter PVP",
       creator: "Riot Games",
       imgURL: "https://cdn2.steamgriddb.com/file/sgdb-cdn/logo_thumb/9ebd41e6cbc1e14780805f6fc0d65867.png",
-      type: "Game"
+      type: "Game",
+      tags: "gaming"
+      
     },
     {
       id: 3,
@@ -33,31 +34,40 @@ export class AppComponent {
       description: "An Online 5 Moba PVP Game",
       creator: "Riot Games",
       imgURL: "https://preview.redd.it/itq8rpld8va51.png?width=256&format=png&auto=webp&s=9701ba6228c29bf2d7e3dfffd45b9a3562507289",
-      type: "Game"
+      type: "Game",
+      tags: "gaming"
     },
     {
       id: 4,
       title: "Dota 2",
       description: "An Online 5 Moba PVP Game",
       creator: "Valve Corporation",
-      imgURL: "https://preview.redd.it/itq8rpld8va51.png?width=256&format=png&auto=webp&s=9701ba6228c29bf2d7e3dfffd45b9a3562507289",
-      type: "Game"
+      imgURL: "https://cdn-icons-png.flaticon.com/512/871/871366.png",
+      type: "Game",
+      tags: "gaming"
     },
     {
       id: 5,
       title: "Genshin Impact",
       description: "An Open World action RPG",
       creator: "Mihoyo",
-      imgURL: "https://preview.redd.it/itq8rpld8va51.png?width=256&format=png&auto=webp&s=9701ba6228c29bf2d7e3dfffd45b9a3562507289",
-      type: "Game"
+      imgURL: "https://preview.redd.it/fi4oa3o2w7t51.png?auto=webp&s=89f13a8454a198909a57288b40c277e4eb677524",
+      type: "Game",
+      tags: "gaming"
     },
     {
       id: 6,
       title: "Grand Theft Auto V",
       description: "An action-adventure game",
       creator: "Rockstar Games",
-      imgURL: "https://preview.redd.it/itq8rpld8va51.png?width=256&format=png&auto=webp&s=9701ba6228c29bf2d7e3dfffd45b9a3562507289",
-      type: "Game"
-    }];
-   }
+      imgURL: "https://image.pngaaa.com/575/2397575-middle.png",
+      type: "Game",
+      tags: "gaming"
+    },
+     ] 
+     constructor() {}
+
+  ngOnInit(): void {
+  }
+
 }
