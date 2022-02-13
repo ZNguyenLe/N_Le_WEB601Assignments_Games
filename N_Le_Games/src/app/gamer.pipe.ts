c5ffdc32127d386b5e46d4cf3a62e866e3aa4c27
ZNguyenLe <nguyen.le@stclairconnect.ca>
2022-02-12 20:45:31 -0500

    trying to figure out filter thing

--- N_Le_Games/src/app/gamer.pipe.ts
+++ N_Le_Games/src/app/gamer.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from '../app/helper-files/content-interface'
 
 @Pipe({
   name: 'gamer'
 })
 export class GamerPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(gameList: Content[], ...args: unknown[]): unknown {
+    return gameList.filter(g => 
+      g.tags != null ? g.tags.length !=0 : false 
+      );
   }
 
 }
