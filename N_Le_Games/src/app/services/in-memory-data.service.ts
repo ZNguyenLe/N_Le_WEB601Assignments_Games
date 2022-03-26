9e83717be55234310c777a8a2f555ece060526b7
ZNguyenLe <nguyen.le@stclairconnect.ca>
2022-03-25 21:15:50 -0400

    added code to do ID stuff for the memory service thing

--- N_Le_Games/src/app/services/in-memory-data.service.ts
+++ N_Le_Games/src/app/services/in-memory-data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Content } from '../helper-files/content-interface';
+import { LOTSOFGAMES } from '../helper-files/contentDb';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
 
   constructor() { }
+
+  createDb() {
+    const game: Content[] = LOTSOFGAMES;
+    return {game};
+  }
+
+  generateID(content: Content[]): number {
+    return content.length > 0 ?
+    Math.max(...content.map(c => c.id ?? 0)) + 1 : 2000;
+  }
 }
