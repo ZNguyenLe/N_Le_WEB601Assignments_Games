f83da133de25b152313599adf892bf4da0dc98c5
ZNguyenLe <nguyen.le@stclairconnect.ca>
2022-04-16 14:25:13 -0400

    added another component to check updates if available

---
+++ N_Le_Games/src/app/log-update.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogUpdateService {
+
+  constructor(private update: SwUpdate) { }
+
+  public init() {
+    this.update.versionUpdates.subscribe(event => {
+      console.log('check changes if there is any');
+      switch (event.type) {
+        case 'VERSION_DETECTED':
+          console.log(`lemme download the new version for ya:  ${event.version.hash }`);
+          break;
+          case 'VERSION_READY': console.log(`Current Version: ${event.currentVersion.hash}`);
+          console.log(`gotchu a new update ready: ${event.latestVersion.hash}`);
+          this.update.activateUpdate().then(() => document.location.reload());
+          break;
+      }
+    });
+  }
+}
