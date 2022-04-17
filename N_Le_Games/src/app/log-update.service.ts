0086ac9f0964c901356aa1df5575cae647a62b93
ZNguyenLe <nguyen.le@stclairconnect.ca>
2022-04-17 02:07:29 -0400

    more testing...

--- N_Le_Games/src/app/log-update.service.ts
+++ N_Le_Games/src/app/log-update.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogUpdateService {
+  constructor(private update: SwUpdate
+              // private snackBar: MatSnackBar
+              ) { }
 
-  constructor(private update: SwUpdate) { }
-
-  
+  // openSnack(message: string, action: string) {
+  //   this.snackBar.open(message, action);
+  // }
   
   public init() {
     this.update.versionUpdates.subscribe(event => {
@@ -19,7 +23,8 @@ export class LogUpdateService {
         case 'VERSION_DETECTED':
           console.log(`lemme download the new version for ya:  ${event.version.hash }`);
           break;
-          case 'VERSION_READY': console.log(`Current Version: ${event.currentVersion.hash}`);
+        case 'VERSION_READY': 
+          console.log(`Current Version: ${event.currentVersion.hash}`);
           console.log(`gotchu a new update ready: ${event.latestVersion.hash}`);
           this.update.activateUpdate().then(() => document.location.reload());
           break;
